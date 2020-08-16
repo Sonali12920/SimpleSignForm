143c9e9af79a89dfbf9cde74623492eb9005db03
Sonali <sonali023btit18@igdtuw.ac.in>
2020-08-16 10:17:09 +0000

    Files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const mongo_username = process.env.MONGO_USERNAME
 const mongo_password = process.env.MONGO_PASSWORD
 
-const uri ='mongodb+srv://Sonali12920:Sonali12920@simpleregistrationform.ktwdm.gcp.mongodb.net/SimpleRegForm?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://Sonali12920:Sonali12920@simpleregistrationform.ktwdm.gcp.mongodb.net/SimpleRegForm?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 let express = require('express');//requiring express model
@@ -13,6 +13,9 @@ let http = require('http').Server(app);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+
 app.get('/', function(req, res) {
   res.sendFile('/index.html', { root: '.' });
 });
@@ -21,7 +24,7 @@ app.post('/create', function(req, res, next) {
   client.connect(err => {
     const students = client.db("crmdb").collection("students");
 
-    let newstudent = {  email: req.body.email, password: req.body.password, name: req.body.name, address: req.body.address, city: req.body.city, state: req.body.state };
+    let newstudent = { email: req.body.email, password: req.body.password, name: req.body.name, address: req.body.address, city: req.body.city, state: req.body.state };
     students.insertOne(newstudent, function(err, res) {
       if (err) throw err;
       console.log("Successfully submitted");
@@ -38,4 +41,6 @@ http.listen(app.get('port'), function() {
 app.engine('pug', require('pug').__express)
 app.set('views', '.')
 app.set('view engine', 'pug')
+
+
 
